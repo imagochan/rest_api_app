6828cc2e4fde36bb7d8935e48afe0342a659cb44
Julio <julioalfredomachado@gmail.com>
2023-08-16 16:40:56 -0600

    added more fields to user model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,4 +1,15 @@
 class User {
   final String gender;
-  User({required this.gender});
+  final String email;
+  final String phone;
+  final String cell;
+  final String nat;
+  
+  User({
+    required this.gender,
+    required this.email ,
+    required this.phone ,
+    required this.cell ,
+    required this.nat ,
+    });
 }
