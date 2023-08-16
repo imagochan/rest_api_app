c7f061bd9dcca46fabe4996966ce7d9d18d5a7f7
Julio <julioalfredomachado@gmail.com>
2023-08-16 15:22:12 -0600

    added app bar title

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -11,7 +11,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Rest API Call'),
+      ),
     );
   }
 }
