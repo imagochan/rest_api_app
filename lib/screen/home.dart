7a2c11b2d452d17b16d53b1a0ee360c5b7ba83e4
Julio <julioalfredomachado@gmail.com>
2023-08-16 18:26:23 -0600

    moving fetch logic to services folder

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,10 @@
-import 'dart:convert';
+// import 'dart:convert';
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
 
-import 'package:http/http.dart' as http;
-import 'package:rest_api_app/model/user_name.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:rest_api_app/model/user_name.dart';
 
 import '../model/user.dart';
 //imported http package from pub.dev
@@ -46,28 +46,28 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Future<void> fetchUsers() async {
-    print('fetchUsers called');
-    final url = 'https://randomuser.me/api/?results=15';
-    final uri = Uri.parse(url);
-    final response = await http.get(uri);
-    final body = response.body;
-    final json = jsonDecode(body);
-    final results = json['results'] as List<dynamic>;//because it contains different types
-    final transformed = results.map((e) {//map results list to transformed list using the user model
-        final name = UserName(title: e['name']['title'], first: e['name']['first'], last: e['name']['last']);
-        return User(
-          cell: e['cell'],
-          email: e['email'],
-          gender: e['gender'],
-          nat: e['nat'],
-          phone: e['phone'],
-          name: name
-        );
-      }).toList();
-    setState(() {
-      users = transformed;
-    });
-    print('fetchUsers completed');
-  }
+  // <Future<void> fetchUsers() async {
+  //   print('fetchUsers called');
+  //   final url = 'https://randomuser.me/api/?results=15';
+  //   final uri = Uri.parse(url);
+  //   final response = await http.get(uri);
+  //   final body = response.body;
+  //   final json = jsonDecode(body);
+  //   final results = json['results'] as List<dynamic>;//because it contains different types
+  //   final transformed = results.map((e) {//map results list to transformed list using the user model
+  //       final name = UserName(title: e['name']['title'], first: e['name']['first'], last: e['name']['last']);
+  //       return User(
+  //         cell: e['cell'],
+  //         email: e['email'],
+  //         gender: e['gender'],
+  //         nat: e['nat'],
+  //         phone: e['phone'],
+  //         name: name
+  //       );
+  //     }).toList();
+  //   setState(() {
+  //     users = transformed;
+  //   });
+  //   print('fetchUsers completed');
+  // }
 }
---
+++ lib/services/user_api.dart
@@ -0,0 +1,29 @@
+//this file will hold the app logic
+//because screens should not have code that deals with logic
+
+class UserApi {
+  Future<void> fetchUsers() async {
+    print('fetchUsers called');
+    final url = 'https://randomuser.me/api/?results=15';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    final results = json['results'] as List<dynamic>;//because it contains different types
+    final transformed = results.map((e) {//map results list to transformed list using the user model
+        final name = UserName(title: e['name']['title'], first: e['name']['first'], last: e['name']['last']);
+        return User(
+          cell: e['cell'],
+          email: e['email'],
+          gender: e['gender'],
+          nat: e['nat'],
+          phone: e['phone'],
+          name: name
+        );
+      }).toList();
+    setState(() {
+      users = transformed;
+    });
+    print('fetchUsers completed');
+  }
+}
