adffe8737648b4c453203a2debdb4a96023b1572
Julio <julioalfredomachado@gmail.com>
2023-08-17 00:04:52 -0600

    removed some comments

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,14 +1,6 @@
-// import 'dart:convert';
-//import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:rest_api_app/services/user_api.dart';
-
-// import 'package:http/http.dart' as http;
-// import 'package:rest_api_app/model/user_name.dart';
-
 import '../model/user.dart';
-//imported http package from pub.dev
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -53,29 +45,4 @@ class _HomeScreenState extends State<HomeScreen> {
       users = response;
     });
   }
-
-  // <Future<void> fetchUsers() async {
-  //   print('fetchUsers called');
-  //   final url = 'https://randomuser.me/api/?results=15';
-  //   final uri = Uri.parse(url);
-  //   final response = await http.get(uri);
-  //   final body = response.body;
-  //   final json = jsonDecode(body);
-  //   final results = json['results'] as List<dynamic>;//because it contains different types
-  //   final transformed = results.map((e) {//map results list to transformed list using the user model
-  //       final name = UserName(title: e['name']['title'], first: e['name']['first'], last: e['name']['last']);
-  //       return User(
-  //         cell: e['cell'],
-  //         email: e['email'],
-  //         gender: e['gender'],
-  //         nat: e['nat'],
-  //         phone: e['phone'],
-  //         name: name
-  //       );
-  //     }).toList();
-  //   setState(() {
-  //     users = transformed;
-  //   });
-  //   print('fetchUsers completed');
-  // }
 }
