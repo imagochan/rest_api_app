f2b3d7f6c8b5c81d4ab6d0292ea1b61ea5e99293
Julio <julioalfredomachado@gmail.com>
2023-08-17 00:14:46 -0600

    added fullname getter

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -8,13 +8,15 @@ class User {
   final String nat;
   final UserName name;
 
-  User({
-    required this.gender,
-    required this.email ,
-    required this.phone ,
-    required this.cell ,
-    required this.nat ,
-    required this.name
-    });
-}
+  User(
+      {required this.gender,
+      required this.email,
+      required this.phone,
+      required this.cell,
+      required this.nat,
+      required this.name});
 
+  String get fullName {
+    return '${name.title} ${name.first} ${name.last}';
+  }
+}
